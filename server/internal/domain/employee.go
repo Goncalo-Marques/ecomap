9b1ea25a7712f4dee8d6ec8bd4c36441389ea043
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-03-10 01:49:43 +0000

    feat: add domain package

---
+++ server/internal/domain/employee.go
@@ -0,0 +1,24 @@
+package domain
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrEmployeeNotFound = errors.New("employee not found") // Returned when an employee is not found.
+)
+
+// EditableEmployee defines the editable employee structure.
+type EditableEmployee struct {
+	Name        string
+	DateOfBirth time.Time
+}
+
+// Employee defines the employee structure.
+type Employee struct {
+	EditableEmployee
+	ID uuid.UUID
+}
