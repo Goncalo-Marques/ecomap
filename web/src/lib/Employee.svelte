a934ba42ec43e699f5ad8de73731ac7a1c6a77d2
joaotomaspinheiro <joaotomasp@gmail.com>
2024-03-13 22:18:44 +0000

    feat: add api proxy

---
+++ web/src/lib/Employee.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	let inputValue: string;
+	let promise: Promise<any>;
+
+	async function getEmployee(employeeId: string) {
+		const res = await fetch(`/api/employees/${employeeId}`);
+		const body = await res.json();
+
+		if (res.ok) {
+			return body;
+		} else {
+			throw new Error(body.message);
+		}
+	}
+
+	function handleClick() {
+		promise = getEmployee(inputValue);
+	}
+</script>
+
+<p>
+	<label for="employeeId">Employee ID</label>
+	<input id="employeeId" type="text" bind:value={inputValue} />
+</p>
+
+<button on:click={handleClick}>Search</button>
+
+{#await promise}
+	<p>Loading...</p>
+{:then employee}
+	{#if employee}
+		<p>{JSON.stringify(employee, null, 2)}</p>
+	{/if}
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
+
+<style>
+	button {
+		width: fit-content;
+	}
+</style>
