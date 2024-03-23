9f7494818ef6314e851e9d507390ee4fc0be690b
João Pinheiro <joaotomasp@gmail.com>
2024-03-23 20:48:45 +0000

    feat: create login page (#39)
    Refs: closes #35 
    
    ## Summary
    
    Create login page.
    
    ---------
    
    Co-authored-by: Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>

---
+++ web/src/lib/components/Button.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from "svelte/elements";
+
+	type Variant = "primary" | "secondary" | "tertiary";
+	type Size = "medium" | "large";
+
+	/**
+	 * A space-separated list of the classes of the element.
+	 * @default ""
+	 */
+	let className: HTMLButtonAttributes["class"] = "";
+	export { className as class };
+
+	/**
+	 * Prevents the user from interacting with the button: it cannot be pressed or focused.
+	 * @default false
+	 */
+	export let disabled: boolean = false;
+
+	/**
+	 * The size of the button.
+	 * @default "medium"
+	 */
+	export let size: Size = "medium";
+
+	/**
+	 * The type of the button.
+	 * @default "button"
+	 */
+	export let type: HTMLButtonAttributes["type"] = "button";
+
+	/**
+	 * The variant of the button.
+	 * @default "primary"
+	 */
+	export let variant: Variant = "primary";
+</script>
+
+<button class={`${size} ${variant} ${className}`} {disabled} {type}>
+	<slot />
+</button>
+
+<style>
+	button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 0.25rem;
+		font-weight: 600;
+		border-radius: 0.25rem;
+		transition-property: var(--transition-colors);
+		transition-duration: var(--transition-duration);
+		transition-timing-function: var(--transition-timing-function);
+
+		&:disabled {
+			opacity: 0.6;
+		}
+	}
+
+	.primary {
+		color: var(--white);
+		background-color: var(--blue-700);
+
+		&:hover:enabled {
+			background-color: var(--blue-800);
+		}
+
+		&:active:enabled {
+			background-color: var(--blue-900);
+		}
+	}
+	.secondary {
+		color: var(--blue-700);
+		background-color: transparent;
+		border: 1px solid var(--blue-700);
+
+		&:hover:enabled {
+			color: var(--blue-800);
+			border-color: var(--blue-800);
+			background-color: var(--blue-100);
+		}
+
+		&:active:enabled {
+			color: var(--blue-800);
+			border-color: var(--blue-800);
+			background-color: var(--blue-200);
+		}
+	}
+	.tertiary {
+		color: var(--blue-700);
+		background-color: transparent;
+
+		&:hover:enabled {
+			color: var(--blue-800);
+			background-color: var(--blue-100);
+		}
+
+		&:active:enabled {
+			color: var(--blue-800);
+			background-color: var(--blue-200);
+		}
+	}
+
+	.medium {
+		padding: 0.5rem 0.75rem;
+	}
+	.large {
+		padding: 0.625rem 1rem;
+	}
+</style>
