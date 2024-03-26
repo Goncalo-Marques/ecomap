ba54b5b2e3f6529f7c11d2bb96d62be0da92cfbd
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-03-26 01:42:10 +0000

    feat: add initial database migration (#42)
    Refs: closes #38 
    
    ## Summary
    
    Add initial database migration.
    
    ## Changes
    
    - Add migration v1
    - Add new words to cspell ignore
    
    ## Followed conventions
    
    - https://www.codecademy.com/learn/fscp-designing-relational-databases/modules/fscp-designing-a-database/cheatsheet
    - https://stackoverflow.com/a/4108266

---
+++ server/database/migrations/000001_initial_database.down.sql
@@ -0,0 +1,44 @@
+-- Route employees.
+DROP TABLE public.routes_employees;
+DROP TYPE public.routes_employees_employee_role;
+
+-- Route containers.
+DROP TABLE public.routes_containers;
+
+-- Routes.
+DROP TABLE public.routes;
+
+-- Warehouse trucks.
+DROP TABLE public.warehouses_trucks;
+
+-- Warehouses.
+DROP TABLE public.warehouses;
+
+-- Trucks.
+DROP TABLE public.trucks;
+
+-- User container bookmarks.
+DROP TABLE public.users_container_bookmarks;
+
+-- Container reports.
+DROP TABLE public.containers_reports;
+DROP TYPE public.containers_reports_issue_type;
+
+-- Containers.
+DROP TABLE public.containers;
+DROP TYPE public.containers_category;
+
+-- Employees.
+DROP TABLE public.employees;
+DROP TYPE public.employees_role;
+
+-- Users.
+DROP TABLE public.users;
+
+-- Functions.
+DROP FUNCTION public.enforce_lower_case_username();
+DROP FUNCTION public.update_modified_time();
+
+-- Extensions.
+DROP EXTENSION pgrouting CASCADE;
+DROP EXTENSION postgis CASCADE;
