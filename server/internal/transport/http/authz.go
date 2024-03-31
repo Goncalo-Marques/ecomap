964641ca1115d2a7917bfdd0ce53ffcbcb8209bc
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-03-31 20:18:31 +0100

    refactor: remove unused const

--- server/internal/transport/http/authz.go
+++ server/internal/transport/http/authz.go
@@ -5,15 +5,8 @@ import (
 	"github.com/goncalo-marques/ecomap/server/internal/authz"
 )
 
-// API roles.
-const (
-	roleUser          = string(authn.SubjectRoleUser)
-	roleWasteOperator = string(authn.SubjectRoleWasteOperator)
-	roleManager       = string(authn.SubjectRoleManager)
-)
-
 // AuthzRoles defines the authorization roles for the API.
 var AuthzRoles authz.Roles = authz.Roles{
 	AuthzWildcards: []string{"employeeId", "userId"},
-	AdminRole:      roleManager,
+	AdminRole:      string(authn.SubjectRoleManager),
 }
