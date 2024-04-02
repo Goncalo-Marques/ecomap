803c961cbee90dbab8c9b8ea029cdae71295291e
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-04-02 17:35:05 +0100

    feat: add map fragment

--- android/app/src/main/java/com/ecomap/ecomap/MapFragment.kt
+++ android/app/src/main/java/com/ecomap/ecomap/MapFragment.kt
@@ -2,4 +2,8 @@ package com.ecomap.ecomap
 
 import androidx.fragment.app.Fragment
 
-class MapFragment : Fragment(R.layout.activity_main)
+/**
+ * Simple fragment to contain the SupportMapFragment. A fragment for managing the lifecycle of a
+ * GoogleMap object.
+ */
+class MapFragment : Fragment()
