fc11e84d3b9dafe159224b3d309714de41940258
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-04-03 23:09:01 +0100

    feat: add camera bounds

--- android/app/src/main/java/com/ecomap/ecomap/MainActivity.kt
+++ android/app/src/main/java/com/ecomap/ecomap/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mapLatLngBound: LatLngBounds
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,8 +27,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             insets
         }
 
+        // Google Map configurations.
+        mapLatLngBound = LatLngBounds(
+            LatLng(
+                resources.getInteger(R.integer.camera_bound_southwest_latitude).toDouble(),
+                resources.getInteger(R.integer.camera_bound_southwest_longitude).toDouble()
+            ),
+            LatLng(
+                resources.getInteger(R.integer.camera_bound_northeast_latitude).toDouble(),
+                resources.getInteger(R.integer.camera_bound_northeast_longitude).toDouble()
+            )
+        )
+
+        val googleMapOptions = GoogleMapOptions()
+        googleMapOptions
+            .mapType(GoogleMap.MAP_TYPE_NORMAL)
+            .latLngBoundsForCameraTarget(mapLatLngBound)
+
         // Add support map fragment to the map container.
-        val mapFragment = SupportMapFragment.newInstance()
+        val mapFragment = SupportMapFragment.newInstance(googleMapOptions)
         supportFragmentManager
             .beginTransaction()
             .add(R.id.fragment_container_view_map, mapFragment)
@@ -34,9 +56,13 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.moveCamera(
+            CameraUpdateFactory.newLatLngBounds(mapLatLngBound, 0)
+        )
+
         googleMap.addMarker(
             MarkerOptions()
-                .position(LatLng(0.0, 0.0))
+                .position(LatLng(40.0, -9.0))
                 .title("Marker")
         )
     }
