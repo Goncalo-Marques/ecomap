8a4db45dc509f1aa83656e4f77cdab6f3c57444d
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-04-04 11:10:10 +0100

    Revert "wip"
    This reverts commit 8702e87aa6ce383b01d90b7ca1685272b2cc68ae.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,8 +51,6 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.play.services.maps) // Maps SDK for Android
-    implementation(libs.play.services.location)
-    implementation(libs.places)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
