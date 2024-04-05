0e4d9e456bef3c7014a389903ac9c86d186d5d2a
joaotomaspinheiro <joaotomasp@gmail.com>
2024-04-05 23:25:28 +0100

    feat: support locale selection

---
+++ web/src/routes/backOffice/components/sidebar/LocaleSelect.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { getSupportedLocale, locale } from "../../../../lib/utils/i8n";
+
+	/**
+	 * Handles on change event of the locale select input.
+	 * @param e Select on change event.
+	 */
+	function handleLocaleChange(e: Event) {
+		const select = e.currentTarget as HTMLSelectElement;
+		locale.set(select.value);
+	}
+</script>
+
+<select value={$locale} on:change={handleLocaleChange}>
+	<option value="en">English</option>
+	<option value="pt">Português</option>
+</select>
+
+<style>
+	select {
+		border: 1px solid var(--gray-300);
+		padding: 0.5rem 0.75rem;
+		border-radius: 0.25rem;
+	}
+</style>
