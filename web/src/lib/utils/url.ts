8cc637e846f1aab8907e7fdef984a12a6bbaeccb
joaotomaspinheiro <joaotomasp@gmail.com>
2024-04-24 21:32:49 +0100

    feat: clear container URL search params after leaving containers page

--- web/src/lib/utils/url.ts
+++ web/src/lib/utils/url.ts
@@ -3,9 +3,12 @@
  * @param searchParams Search params to be set in the URL.
  */
 export function updateSearchParams(searchParams: URLSearchParams) {
-	history.pushState(
-		null,
-		"",
-		`${location.origin}${location.pathname}?${searchParams}`,
-	);
+	history.pushState(null, "", `${location.pathname}?${searchParams}`);
+}
+
+/**
+ * Clears search params from the current URL.
+ */
+export function clearSearchParams() {
+	history.pushState(null, "", location.pathname);
 }
--- web/src/routes/backOffice/containers/Containers.svelte
+++ web/src/routes/backOffice/containers/Containers.svelte
@@ -4,14 +4,18 @@
 	import Card from "../components/Card.svelte";
 	import ContainersTable from "./list/ContainersTable.svelte";
 	import containersStore, { initialFilters } from "./list/containersStore";
+	import { clearSearchParams } from "../../../lib/utils/url";
 
 	onMount(() => {
 		containersStore.fetchData();
 	});
 
 	onDestroy(() => {
-		// Reset containers filters to its initial state when component is destroyed.
+		// Reset containers filters to its initial state.
 		containersStore.filters.set(initialFilters);
+
+		// Clear containers filters search params from URL.
+		clearSearchParams();
 	});
 </script>
 
