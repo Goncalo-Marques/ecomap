0d860d9e1ac951771107e9ca1356cd6d3061adc5
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-04-25 15:56:21 +0100

    feat: update road network query

--- server/internal/store/roadnetwork.go
+++ server/internal/store/roadnetwork.go
@@ -11,16 +11,15 @@ import (
 	"github.com/goncalo-marques/ecomap/server/internal/domain"
 )
 
-// GetRoadIDByGeometry executes a query to return the identifier of the road that is closest to the given geometry in
-// the road network.
-func (s *store) GetRoadIDByGeometry(ctx context.Context, tx pgx.Tx, geometry domain.GeoJSONGeometryPoint) (int, error) {
+// GetRoadByGeometry executes a query to return the road that is closest to the given geometry in the road network.
+func (s *store) GetRoadByGeometry(ctx context.Context, tx pgx.Tx, geometry domain.GeoJSONGeometryPoint) (domain.Road, error) {
 	geoJSON, err := json.Marshal(geometry)
 	if err != nil {
-		return 0, fmt.Errorf("%s: %w", descriptionFailedMarshalGeoJSON, err)
+		return domain.Road{}, fmt.Errorf("%s: %w", descriptionFailedMarshalGeoJSON, err)
 	}
 
 	row := tx.QueryRow(ctx, `
-		SELECT rn.id
+		SELECT rn.id, rn.osm_id, rn.osm_name, rn.osm_meta, rn.osm_source_id, rn.osm_target_id, rn.clazz, rn.flags, rn.source, rn.target, rn.km, rn.kmh, rn.cost, rn.reverse_cost, rn.x1, rn.y1, rn.x2, rn.y2
 		FROM pgr_findCloseEdges(
 			$$SELECT id, geom_way as geom FROM road_network$$,
 			ST_GeomFromGeoJSON($1),
@@ -31,15 +30,35 @@ func (s *store) GetRoadIDByGeometry(ctx context.Context, tx pgx.Tx, geometry dom
 		string(geoJSON),
 	)
 
-	var id int
-	err = row.Scan(&id)
+	var road domain.Road
+
+	err = row.Scan(
+		&road.ID,
+		&road.OsmID,
+		&road.OsmName,
+		&road.OsmMeta,
+		&road.OsmSourceID,
+		&road.OsmTargetID,
+		&road.Clazz,
+		&road.Flags,
+		&road.Source,
+		&road.Target,
+		&road.KM,
+		&road.KMH,
+		&road.Cost,
+		&road.ReverseCost,
+		&road.X1,
+		&road.Y1,
+		&road.X2,
+		&road.Y2,
+	)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
-			return 0, fmt.Errorf("%s: %w", descriptionFailedScanRow, domain.ErrRoadNotFound)
+			return domain.Road{}, fmt.Errorf("%s: %w", descriptionFailedScanRow, domain.ErrRoadNotFound)
 		}
 
-		return 0, fmt.Errorf("%s: %w", descriptionFailedScanRow, err)
+		return domain.Road{}, fmt.Errorf("%s: %w", descriptionFailedScanRow, err)
 	}
 
-	return id, nil
+	return road, nil
 }
