f022395e4de6da3358565d7f710d773bcb65478f
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-05-06 15:12:54 +0100

    chore: add default admin employee migration (#159)
    ## Summary
    
    Update the migration 1 to insert a default admin employee.

--- server/database/migrations/000001_initial_database.up.sql
+++ server/database/migrations/000001_initial_database.up.sql
@@ -73,6 +73,10 @@ CREATE TRIGGER employees_update_modified_at
     FOR EACH ROW
     EXECUTE PROCEDURE update_modified_at();
 
+-- Insert default admin manager employee with 'admin' password.
+INSERT INTO employees(username, password, first_name, last_name, role, date_of_birth, phone_number, geom, schedule_start, schedule_end)
+    VALUES ('admin', '$2a$14$3YbWepKf4uralK8RS4Gi5eiIwKOrUU0dHWXaTCGpEmWfr.gdjEu96', 'Super', 'Admin', 'manager', '1970-01-01', '', ST_GeomFromGeoJSON('{"coordinates": [0, 0], "type": "Point"}'), '00:00:00', '00:00:00');
+
 -- Containers.
 CREATE TYPE containers_category AS ENUM ('general', 'paper', 'plastic', 'metal', 'glass', 'organic', 'hazardous');
 
