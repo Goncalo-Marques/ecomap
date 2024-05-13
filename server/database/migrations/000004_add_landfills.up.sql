19857179f9b699227339f60b99cb0a497bbfe342
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-05-13 23:27:53 +0100

    fix: update not null constraint

--- server/database/migrations/000004_add_landfills.up.sql
+++ server/database/migrations/000004_add_landfills.up.sql
@@ -2,8 +2,8 @@
 CREATE TABLE landfills (
     id              uuid                    NOT NULL    DEFAULT GEN_RANDOM_UUID(),
     geom            geometry('POINT', 4326) NOT NULL,
-    road_id         integer                 NOT NULL,
-    municipality_id integer                 NOT NULL,
+    road_id         integer,
+    municipality_id integer,
     created_at      timestamp               NOT NULL    DEFAULT CURRENT_TIMESTAMP,
     modified_at     timestamp               NOT NULL    DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT landfills_pkey                   PRIMARY KEY (id),
