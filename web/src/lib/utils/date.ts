e92cde896a9867bdb4b9348d5b62259a85c5eacf
Gonçalo <goncalo.gjad@gmail.com>
2024-05-16 18:38:14 +0100

    refactor: suggestions in code review

--- web/src/lib/utils/date.ts
+++ web/src/lib/utils/date.ts
@@ -3,9 +3,9 @@ import { locale } from "./i8n";
 import { DateFormats } from "../constants/date";
 
 /**
- * Date used to format times.
+ * Date the Unix time 0.
  */
-const UNIX_TIME = "1970-01-01";
+const UNIX_TIME_0_DATE = "1970-01-01";
 
 /**
  * Formats a date into a given format.
@@ -29,5 +29,5 @@ export function formatDate(
  * @returns Formatted time.
  */
 export function formatTime(time: string): string {
-	return formatDate(`${UNIX_TIME} ${time}`, DateFormats.shortTime);
+	return formatDate(`${UNIX_TIME_0_DATE} ${time}`, DateFormats.shortTime);
 }
