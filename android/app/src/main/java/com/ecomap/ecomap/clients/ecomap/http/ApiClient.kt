8c987c25bc75dd8aeaedec34cd70557b829e23d8
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-05-18 15:18:17 +0100

    refactor: replace field names with const

--- android/app/src/main/java/com/ecomap/ecomap/clients/ecomap/http/ApiClient.kt
+++ android/app/src/main/java/com/ecomap/ecomap/clients/ecomap/http/ApiClient.kt
@@ -9,22 +9,28 @@ import com.ecomap.ecomap.domain.User
 import org.json.JSONException
 import org.json.JSONObject
 
-const val BASE_API_URL = "https://server-7fzc7ivuwa-ew.a.run.app/api"
-
 /**
  * EcoMap HTTP API client.
  */
 object ApiClient {
     private val LOG_TAG = ApiClient::class.java.simpleName
 
-    /**
-     * Retrieves the URL of the API.
-     * @param endpoint API endpoint.
-     * @return API URL.
-     */
-    private fun getApiUrl(endpoint: String): String {
-        return BASE_API_URL + endpoint
-    }
+    // URLs.
+    private const val URL_BASE = "https://server-7fzc7ivuwa-ew.a.run.app/api"
+    private const val URL_USERS = "$URL_BASE/users"
+    private const val URL_USERS_SIGN_IN = "$URL_BASE/users/signin"
+
+    // Authentication field names.
+    private const val FIELD_NAME_TOKEN = "token"
+
+    // User field names.
+    private const val FIELD_NAME_ID = "id"
+    private const val FIELD_NAME_USERNAME = "username"
+    private const val FIELD_NAME_PASSWORD = "password"
+    private const val FIELD_NAME_FIRST_NAME = "firstName"
+    private const val FIELD_NAME_LAST_NAME = "lastName"
+    private const val FIELD_NAME_CREATED_AT = "createdAt"
+    private const val FIELD_NAME_MODIFIED_AT = "modifiedAt"
 
     /**
      * Signs in a user with a given username and password.
@@ -41,15 +47,15 @@ object ApiClient {
         errorListener: ErrorListener
     ): JsonObjectRequest {
         val requestPayload = JSONObject()
-        requestPayload.put("username", username)
-        requestPayload.put("password", password)
+        requestPayload.put(FIELD_NAME_USERNAME, username)
+        requestPayload.put(FIELD_NAME_PASSWORD, password)
 
         return JsonObjectRequest(
-            Request.Method.POST, getApiUrl("/users/signin"), requestPayload,
+            Request.Method.POST, URL_USERS_SIGN_IN, requestPayload,
             { response ->
                 var token: String? = null
                 try {
-                    token = response.getString("token")
+                    token = response.getString(FIELD_NAME_TOKEN)
                 } catch (e: JSONException) {
                     Log.e(LOG_TAG, e.message, e)
                 }
@@ -79,22 +85,23 @@ object ApiClient {
         errorListener: ErrorListener
     ): JsonObjectRequest {
         val requestPayload = JSONObject()
-        requestPayload.put("firstName", firstName)
-        requestPayload.put("lastName", lastName)
-        requestPayload.put("username", username)
-        requestPayload.put("password", password)
+
+        requestPayload.put(FIELD_NAME_FIRST_NAME, firstName)
+        requestPayload.put(FIELD_NAME_LAST_NAME, lastName)
+        requestPayload.put(FIELD_NAME_USERNAME, username)
+        requestPayload.put(FIELD_NAME_PASSWORD, password)
 
         return JsonObjectRequest(
-            Request.Method.POST, getApiUrl("/users"), requestPayload,
+            Request.Method.POST, URL_USERS, requestPayload,
             { response ->
                 listener.onResponse(
                     User(
-                        response.getString("id"),
-                        response.getString("username"),
-                        response.getString("firstName"),
-                        response.getString("lastName"),
-                        response.getString("createdAt"),
-                        response.getString("modifiedAt")
+                        response.getString(FIELD_NAME_ID),
+                        response.getString(FIELD_NAME_USERNAME),
+                        response.getString(FIELD_NAME_FIRST_NAME),
+                        response.getString(FIELD_NAME_LAST_NAME),
+                        response.getString(FIELD_NAME_CREATED_AT),
+                        response.getString(FIELD_NAME_MODIFIED_AT)
                     )
                 )
             },
