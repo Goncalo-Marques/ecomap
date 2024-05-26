0f1098b4a6fb26ccdcc3c89f9b5b207b0d3dfe3b
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2024-05-26 22:27:56 +0100

    feat: add sign in progress bar

--- android/app/src/main/java/com/ecomap/ecomap/signin/SignInActivity.kt
+++ android/app/src/main/java/com/ecomap/ecomap/signin/SignInActivity.kt
@@ -2,7 +2,9 @@ package com.ecomap.ecomap.signin
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +20,7 @@ import com.google.android.material.textfield.TextInputEditText
 class SignInActivity : AppCompatActivity() {
     private lateinit var textInputEditTextUsername: TextInputEditText
     private lateinit var textInputEditTextPassword: TextInputEditText
+    private lateinit var progressBar: ProgressBar
 
     private lateinit var store: UserStore
 
@@ -39,10 +42,14 @@ class SignInActivity : AppCompatActivity() {
         textInputEditTextPassword = findViewById(R.id.text_input_edit_text_password)
         val buttonSignIn: Button = findViewById(R.id.button_sign_in)
         val buttonCreateAccount: Button = findViewById(R.id.button_create_account)
+        progressBar = findViewById(R.id.progress_bar_sign_in)
 
         // Set up on click events for the sign in and create account button.
         buttonSignIn.setOnClickListener { signInUser() }
         buttonCreateAccount.setOnClickListener { openCreateAccountScreen() }
+
+        // Hide progress bar when activity is created.
+        progressBar.visibility = View.INVISIBLE
     }
 
     /**
@@ -71,6 +78,9 @@ class SignInActivity : AppCompatActivity() {
             return
         }
 
+        // Display progress bar.
+        progressBar.visibility = View.VISIBLE
+
         val request =
             ApiClient.signIn(
                 username,
@@ -84,7 +94,10 @@ class SignInActivity : AppCompatActivity() {
 
                     finishAffinity()
                 },
-                { _ ->
+                {
+                    // Hide the progress bar when a network error occurs.
+                    progressBar.visibility = View.INVISIBLE
+
                     Toast.makeText(
                         applicationContext,
                         getString(R.string.error_sign_in_invalid_credentials),
