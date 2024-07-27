b9f1505f93123425e95d5c1d318fc31ad5030983
joaotomaspinheiro <joaotomasp@gmail.com>
2024-07-27 15:17:03 +0100

    fix: remove commented styles

--- web/src/lib/components/MenuItem.svelte
+++ web/src/lib/components/MenuItem.svelte
@@ -3,30 +3,3 @@
 >
 	<slot />
 </li>
-
-<!-- <style>
-	li {
-		display: flex;
-		align-items: center;
-		gap: 0.5rem;
-		padding: 0.5rem 0.75rem;
-		text-wrap: nowrap;
-		transition-property: var(--transition-colors);
-		transition-duration: var(--transition-duration);
-		transition-timing-function: var(--transition-timing-function);
-
-		& > * {
-			flex: 1;
-		}
-
-		&:hover {
-			color: var(--green-800);
-			background-color: var(--green-50);
-		}
-
-		&:active {
-			color: var(--green-800);
-			background-color: var(--green-100);
-		}
-	}
-</style> -->
